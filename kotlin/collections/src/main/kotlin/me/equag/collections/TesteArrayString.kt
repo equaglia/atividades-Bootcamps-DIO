c9724fba470ea8f6e2363bd74ce2b80bcd2361ea
equaglia <equaglia@gmail.com>
2021-05-05 11:36:15 -0300

    Organizando pastas

---
+++ kotlin/collections/src/main/kotlin/me/equag/collections/TesteArrayString.kt
@@ -0,0 +1,21 @@
+package me.equag.collections
+
+fun main() {
+    val nomes = Array(3){""}
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "José"
+
+    for (nome in nomes){
+        println(nome)
+    }
+
+    println("------------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("------------------------")
+    val nomes2 = arrayOf("Maria", "Zaza", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
---
+++ kotlin/collections/src/main/kotlin/me/equag/collections/TesteColecoesMutaveis.kt
@@ -0,0 +1,37 @@
+package me.equag.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.00, "CLT")
+    val pedro = Funcionario("Pedro", 1500.00, "PJ")
+    val maria = Funcionario("Maria", 4000.00, "CLT")
+
+    val funcionarios = mutableListOf(joao, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("------------------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("------------------------")
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+    println("------------------------")
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach { println(it) }
+
+    println("------------------------")
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+
+    println("------------------------")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach { println(it) }
+
+
+
+
+
+}
